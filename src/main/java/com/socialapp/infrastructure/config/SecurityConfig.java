dd7636a90571cb3d11650bf0fa151e0e7e797c0d
Nguyễn Thanh Trường <honguyententhanhtruong@gmail.com>
2026-04-03 14:03:15 +0700

    update url

--- src/main/java/com/socialapp/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/socialapp/infrastructure/config/SecurityConfig.java
@@ -31,7 +31,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://social-app-eight-eosin.vercel.app"
+        ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
