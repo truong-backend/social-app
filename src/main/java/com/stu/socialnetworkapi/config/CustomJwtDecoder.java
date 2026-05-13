22b82dcf3a78a9ff927953ab4e415278179c3540
Truong back-end <honguyententhanhtruong@gmail.com>
2026-05-13 14:07:46 +0700

    chore: add docker files and env from tracking

--- src/main/java/com/stu/socialnetworkapi/config/CustomJwtDecoder.java
+++ src/main/java/com/stu/socialnetworkapi/config/CustomJwtDecoder.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 
 @Component
 @RequiredArgsConstructor
+
 public class CustomJwtDecoder implements JwtDecoder {
     @Value("${jwt.access-token.key}")
     private String key;
